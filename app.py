0f9d9bee302806477486756422666a6aba5132bc
Code Adventures <code-adventures@web.de>
2024-07-04 21:16:06 +0200

    started general structure, querying all shops and a shop by id works

--- app.py
+++ app.py
@@ -5,6 +5,84 @@ import storage
 app = Flask(__name__)
 
 
+# urls
+# GET /shops                                    all shops
+# GET /shops/{id}                               one shop
+# POST /shops                                   create new shop
+# PUT /shops/{id}                               update shop
+# DELETE /shops/{id}                            delete shop
+
+# GET /products                                 all products
+# GET /products/{id}                            one product
+# POST /products                                create new product
+# PUT /products/{id}                            update product
+# DELETE /products/{id}                         delete product
+# GET /products/{id}/brands                     all brands
+# GET /products/{id}/brands/{brand_id}          one brand
+# POST /products/{id}/brands                    add brand
+# PUT /products/{id}/brands/{brand_id}          update brand
+# DELETE /products/{id}/brands/{brand_id}       delete brand
+
+# GET /storages                                 all storages
+# GET /storages/{id}                            one storage
+# POST /storages                                create new storage
+# PUT /storages/{id}                            update storage
+# DELETE /storages/{id}                         delete storage
+# GET /storages/{id}/entries                    all entries
+# GET /storages/{id}/entries/{entry_id}         one entry
+# POST /storages/{id}/entries                   add entry
+# PUT /storages/{id}/entries/{entry_id}         update entry
+# DELETE /storages/{id}/entries/{entry_id}      delete entry
+# PUT /storages/{id}/entries/{entry_id}/move    move entry
+
+# GET /lists                                    all lists
+# GET /lists/{id}                               one list
+# POST /lists                                   create new list
+# PUT /lists/{id}                               update list
+# DELETE /lists/{id}                            delete list
+# GET /lists/{id}/entries                       all entries
+# GET /lists/{id}/entries/{entry_id}            one entry
+# POST /lists/{id}/entries                      add entry
+# PUT /lists/{id}/entries/{entry_id}            update entry
+# DELETE /lists/{id}/entries/{entry_id}         delete entry
+# PUT /lists/{id}/entries/{entry_id}/move       move entry
+
+@app.route('/shops', methods=['GET'])
+def get_shops():
+    data, code, code_msg = storage.get_shops()
+    if data:
+        return jsonify(data), code
+    return code_msg, code
+
+@app.route('/shops/<int:id>', methods=['GET'])
+def get_shop(id):
+    data, code, code_msg = storage.get_shop(id)
+    if data:
+        return jsonify(data), code
+    return code_msg, code
+
+@app.route('/shops', methods=['POST'])
+def create_shop():
+    data, code, code_msg = storage.create_shop()
+    if data:
+        return jsonify(data), code
+    return code_msg, code
+
+@app.route('/shops/<int:id>', methods=['PUT'])
+def update_shop(id):
+    data, code, code_msg = storage.update_shop(id)
+    if data:
+        return jsonify(data), code
+    return code_msg, code
+
+@app.route('/shops/<int:id>', methods=['DELETE'])
+def delete_shop(id):
+    data, code, code_msg = storage.delete_shop(id)
+    if data:
+        return jsonify(data), code
+    return code_msg, code
+
+
 @app.route('/')
 def hello_world():
     return jsonify(storage.get_shopping_lists())
--- storage.py
+++ storage.py
@@ -1,10 +1,39 @@
 import orm
 from sqlalchemy import create_engine
+from sqlalchemy import select
 from sqlalchemy.orm import Session
+
+class RETURN:
+    OK = 200
+    CREATED = 201
+    NOT_FOUND = 404
+
+    @staticmethod
+    def ok(data):
+        return data, RETURN.OK, ""
+
+    @staticmethod
+    def not_found(msg):
+        return None, RETURN.NOT_FOUND, msg
 
 
 engine = create_engine('sqlite:///data.db', echo=True)
 orm.Base.metadata.create_all(engine)
+
+def get_shops():
+    with Session(engine) as session:
+        data = []
+        for s in session.scalars(select(orm.Shop)).all():
+            data.append({'id' : s.id, 'name' : s.name})
+        return RETURN.ok(data)
+
+def get_shop(id):
+    with Session(engine) as session:
+        s = session.scalars(select(orm.Shop).where(orm.Shop.id == id)).one_or_none()
+        if s is None:
+            return RETURN.not_found("Shop not found")
+        data = {'id' : s.id, 'name' : s.name}
+        return RETURN.ok(data)
 
 def get_shopping_lists():
     with Session(engine) as session:
